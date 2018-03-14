1fe48d240b12956053680d9d85b3593b98041159
Jelani Brandon <jebrando@microsoft.com>
2018-03-14 09:28:19 -0700

    Update script to run analysis

--- script/execute_analysis.sh
+++ script/execute_analysis.sh
@@ -16,7 +16,7 @@ cmake $repo_root -DCMAKE_BUILD_TYPE=Release -Dskip_samples:BOOL=ON -Duse_prov_cl
 # Set the default cores
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 
-# Make sure there is enough virtual memory on the device to handle more than one job  
+# Make sure there is enough virtual memory on the device to handle more than one job
 MINVSPACE="1500000"
 
 # Acquire total memory and total swap space setting them to zero in the event the command fails
@@ -30,6 +30,7 @@ make --jobs=$CORES
 
 # Run the analysis applications
 ./binary_info/binary_info -c $cmake_folder
-./memory/telemetry_memory/telemetry_memory -c $IOTHUB_CONNECTION_STRING -d jebrandoDevice -k "P3YJYsVQwytQP2JrLca9lSCvyYfPDVjiCZR+5apSQ7c="
-./memory/network_info/network_info -c $IOTHUB_CONNECTION_STRING -d jebrandoDevice -k "P3YJYsVQwytQP2JrLca9lSCvyYfPDVjiCZR+5apSQ7c="
-./memory/provisioning_mem/provisioning_mem -c "global.azure-devices-provisioning.net" -s $PROV_SCOPE_ID
+./memory/telemetry_memory/telemetry_memory -c $IOTHUB_CONNECTION_STRING
+./memory/telemetry_net_info/telemetry_net_info -c $IOTHUB_CONNECTION_STRING
+./memory/prov_net_info/prov_net_info -c $DPS_C_CONNECTION_STRING -s $DPS_C_SCOPE_ID_VALUE
+./memory/provisioning_mem/provisioning_mem -c $DPS_C_CONNECTION_STRING -s $DPS_C_SCOPE_ID_VALUE
