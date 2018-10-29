165afd42013f7174af58141ef590f0db5c184d0d
jebrando <jelani.brandon@microsoft.com>
2018-10-29 16:03:20 -0700

    Updating script to be more concise

--- script/execute_analysis.sh
+++ script/execute_analysis.sh
@@ -4,6 +4,9 @@
 
 set -e
 
+gcc --version
+openssl version
+
 script_dir=$(cd "$(dirname "$0")" && pwd)
 repo_root=$(cd "${script_dir}/.." && pwd)
 cmake_folder=$repo_root"/cmake/analysis_linux"
@@ -15,20 +18,7 @@ mkdir -p $cmake_folder
 pushd $cmake_folder
 cmake $repo_root -DCMAKE_BUILD_TYPE=Release -Duse_prov_client:BOOL=ON
 
-# Set the default cores
-CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
-
-# Make sure there is enough virtual memory on the device to handle more than one job
-MINVSPACE="1500000"
-
-# Acquire total memory and total swap space setting them to zero in the event the command fails
-MEMAR=( $(sed -n -e 's/^MemTotal:[^0-9]*\([0-9][0-9]*\).*/\1/p' -e 's/^SwapTotal:[^0-9]*\([0-9][0-9]*\).*/\1/p' /proc/meminfo) )
-[ -z "${MEMAR[0]##*[!0-9]*}" ] && MEMAR[0]=0
-[ -z "${MEMAR[1]##*[!0-9]*}" ] && MEMAR[1]=0
-
-let VSPACE=${MEMAR[0]}+${MEMAR[1]}
-
-make -j
+make -j --silent
 
 # Run strip from the binaries
 #./binary_info/lower_layer/
--- script/execute_multiple.sh
+++ script/execute_multiple.sh
@@ -4,28 +4,18 @@
 
 set -e
 
+gcc --version
+openssl version
+
 script_dir=$(cd "$(dirname "$0")" && pwd)
 repo_root=$(cd "${script_dir}/.." && pwd)
 cmake_folder=$repo_root"/cmake/analysis_linux"
-
-# Set the default cores
-CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
-
-# Make sure there is enough virtual memory on the device to handle more than one job
-MINVSPACE="1500000"
-
-# Acquire total memory and total swap space setting them to zero in the event the command fails
-MEMAR=( $(sed -n -e 's/^MemTotal:[^0-9]*\([0-9][0-9]*\).*/\1/p' -e 's/^SwapTotal:[^0-9]*\([0-9][0-9]*\).*/\1/p' /proc/meminfo) )
-[ -z "${MEMAR[0]##*[!0-9]*}" ] && MEMAR[0]=0
-[ -z "${MEMAR[1]##*[!0-9]*}" ] && MEMAR[1]=0
-
-let VSPACE=${MEMAR[0]}+${MEMAR[1]}
 
 declare -a arr=(
     "-Dsdk_branch=lts_10_2018"
     "-Dsdk_branch=2018-09-11"
     "-Dsdk_branch=2018-07-11"
-    ""
+    "-Dsdk_branch=master"
 )
 
 for item in "${arr[@]}"
@@ -35,20 +25,13 @@ do
     pushd $cmake_folder
 
     echo "executing cmake/make with options <<$item>>"
-    cmake $repo_root -DCMAKE_BUILD_TYPE=Release -Duse_prov_client:BOOL=ON
+    cmake $repo_root -DCMAKE_BUILD_TYPE=Release -Dsdk_branch="$item"
 
-    make --jobs=$MAKE_CORES
+    # Run make, but don't show the output
+    make -j --silent
 
     # Run the analysis applications
     echo "Retrieving binary info"
     ./binary_info/binary_info -c $cmake_folder
-
-    #echo "retrieving telemetry memory info"
-    #./memory/telemetry_memory/telemetry_memory -c $conn_string
-    #echo "retrieving telemetry network info"
-    #./network/telemetry_net_info/telemetry_net_info -c $conn_string
-
-    #./memory/prov_net_info/prov_net_info -c $DPS_C_CONNECTION_STRING -s $DPS_C_SCOPE_ID_VALUE
-    #./memory/provisioning_mem/provisioning_mem -c $DPS_C_CONNECTION_STRING -s $DPS_C_SCOPE_ID_VALUE
 done
 popd
