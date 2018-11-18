0f6bf3ca8c235760e19b7026c273ee820fe6fe73
jebrando <jebrando@microsoft.com>
2018-11-17 21:30:57 -0800

    Added linux process handler

--- app_analysis/src/process_handler_linux.c
+++ app_analysis/src/process_handler_linux.c
@@ -3,18 +3,52 @@
 
 #include <stdlib.h>
 #include <stdint.h>
-
+#include <unistd.h>
 #include "process_handler.h"
 
 #include "azure_c_shared_utility/crt_abstractions.h"
+#include "azure_c_shared_utility/xlogging.h"
+#include "azure_c_shared_utility/threadapi.h"
 
 typedef struct PROCESS_HANDLER_INFO_TAG
 {
     char* process_filename;
-    __pid_t proc_id;
+    pid_t proc_id;
+    PROCESS_END_CB process_end_cb;
+    void* user_cb;
 } PROCESS_HANDLER_INFO;
 
-PROCESS_HANDLER_HANDLE process_handler_create(const char* process_path)
+#define PID_LINE_LENGTH     1024
+
+static int get_process_id(const char* process_name)
+{
+    int result;
+    char pid_line[PID_LINE_LENGTH];
+    
+    FILE* fp = popen("pidof test_app", "r");
+    if (fp == NULL)
+    {
+        result = 0;
+    }
+    else
+    {
+        char* pid;
+        fgets(pid_line, PID_LINE_LENGTH, fp);
+        if ((pid = strtok(pid_line, " ")) == NULL)
+        {
+
+            result = 0;
+        }
+        else
+        {
+            result = atoi(pid);
+        }
+        pclose(fp);
+    }
+    return result;
+}
+
+PROCESS_HANDLER_HANDLE process_handler_create(const char* process_path, PROCESS_END_CB process_end_cb, void* user_cb)
 {
     PROCESS_HANDLER_INFO* result;
     if (process_path == NULL)
@@ -31,29 +65,12 @@ PROCESS_HANDLER_HANDLE process_handler_create(const char* process_path)
         }
         else
         {
-            char pidline[1024];
-            char *pid;
-            FILE* fp = popen("pidof test_app", "r");
-            int i = 0;
-            int pidno[64];
-            
-            fgets(pidline, 1024, fp);
-            printf("%s", pidline);
-            pid = strtok (pidline, " ");
-            while (pid != NULL)
-            {
-                pidno[i] = atoi(pid);
-                printf("%d\n",pidno[i]);
-                pid = strtok (NULL , " ");
-                i++;
-            }
-            pclose(fp);
         }
     }
     return result;
 }
 
-PROCESS_HANDLER_HANDLE process_handler_create_by_pid(int process_id)
+PROCESS_HANDLER_HANDLE process_handler_create_by_pid(int process_id, PROCESS_END_CB process_end_cb, void* user_cb)
 {
     PROCESS_HANDLER_INFO* result;
     if ((result = (PROCESS_HANDLER_INFO*)malloc(sizeof(PROCESS_HANDLER_INFO))) != NULL)
@@ -81,21 +98,58 @@ int process_handler_start(PROCESS_HANDLER_HANDLE handle, const char* cmdline_arg
     }
     else
     {
-        result = 0;
+        pid_t pid;
+        if ((pid = fork()) == -1)
+        {
+            result = __LINE__;
+        }
+        else if (pid == 0)
+        {
+            exec(handle->process_filename, cmdline_args);
+            exit(0);
+        }
+        else
+        {
+            // Give the executable a chance to start
+            ThreadAPI_Sleep(2000);
+            if ((handle->proc_id = get_process_id(handle->process_filename)) == 0)
+            {
+                result = __LINE__;
+            }
+            else
+            {
+                result = 0;
+            }
+        }
     }
     return result;
 }
 
-uint32_t process_handler_get_bin_size(PROCESS_HANDLER_HANDLE handle)
+int process_handler_end(PROCESS_HANDLER_HANDLE handle)
 {
-    uint32_t result;
+    int result;
     if (handle == NULL)
     {
+        LogError("Invalid argument Process Handler is NULL");
         result = __LINE__;
     }
     else
     {
         result = 0;
+    }
+    return result;
+}
+
+bool process_handler_is_active(PROCESS_HANDLER_HANDLE handle)
+{
+    bool result;
+    if (handle == NULL)
+    {
+        result = false;
+    }
+    else
+    {
+        result = (get_process_id(handle->process_filename) != 0);
     }
     return result;
 }
